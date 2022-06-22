7b1fd0f109d6afb47968c847dab7d4ba8e9638c1
Pawel Wasiluk <pwasi@softserveinc.com>
2022-06-22 15:44:34 +0200

    grpc-services

--- services/coinmarket/cmd/grpc/main.go
+++ services/coinmarket/cmd/grpc/main.go
@@ -4,16 +4,16 @@ import (
 	"net"
 	"os"
 
-	"github.com/Pavlico/topcoin/services/cryptocompare/pkg/grpcserver"
-	protos "github.com/Pavlico/topcoin/services/cryptocompare/pkg/protos/cryptocompare"
+	"github.com/Pavlico/topcoin/services/coinmarket/pkg/grpcserver"
+	protos "github.com/Pavlico/topcoin/services/coinmarket/pkg/protos/coinmarket"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	gs := grpc.NewServer()
-	cs := grpcserver.NewTopList()
-	protos.RegisterCryptocompareServer(gs, cs)
+	cs := grpcserver.NewScoreList()
+	protos.RegisterCoinmarketServer(gs, cs)
 
 	reflection.Register(gs)
 	l, err := net.Listen("tcp", ":8061")
